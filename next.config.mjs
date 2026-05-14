8755a92b649ae5064ae42da7bcc424acf4537248
er-sanjayksw <ss872267@gmail.com>
2026-05-14 15:48:21 -0600

    offline

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,17 @@
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  cacheOnFrontEndNav: true,
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
   serverExternalPackages: ['socket.io'],
+  turbopack: {},
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
